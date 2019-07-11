36bd72de85dba5d4c7337c1f573884d07f1b0851
Ido Nissim <idonnissim@gmail.com>
2019-07-11 18:00:19 +0300

    tweak for clarity in linear_regression usage.

--- ml_implementations/usage_examples/linear_regression.py
+++ ml_implementations/usage_examples/linear_regression.py
@@ -5,7 +5,7 @@ from ml_implementations.supervised_learning.linear_regression import OrdinaryLea
 
 
 def main():
-    X, y = datasets.load_boston(True)
+    X, y = datasets.load_boston(return_X_y=True)
     X = normalize(X)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
